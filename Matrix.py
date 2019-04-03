59225acab027a04551cb4d203879990c3052a356
Michał Piwowarczyk <micpiw16@gmail.com>
2019-04-03 23:18:04 +0200

    better version

--- Matrix.py
+++ Matrix.py
@@ -2,28 +2,104 @@ from math import sqrt
 
 class Matrix(object):
 	@staticmethod 
-	def check_dimension(num_of_arguments):
+	def check_arguments(num_of_arguments):
 		dimension = sqrt(num_of_arguments)
-		print(dimension)
-		print("dim")
 		if dimension.is_integer():
 			return int(dimension)
 		else:
-			raise ValueError ("Wrong number of arguments")
+			raise ValueError("Wrong number of arguments")
+
+	@staticmethod
+	def check_dimension(left, right):
+		if left.dimension != right.dimension:
+			raise ValueError("Wrong dimensions")
 
 	def __init__(self, *values_input):
-		self.dimension = Matrix.check_dimension(len(values_input))
+		self.dimension = Matrix.check_arguments(len(values_input))
 		self.values = []
 		for i in range(self.dimension):
 			self.values.append([j for j in values_input[i*self.dimension:(i+1)*self.dimension]])
 
-	def add(self, matrix):
-		newValues = []
-		for i in range(self.dimension):
-			for j in range(self.dimension):
-				newValues.append(self.values[i][j]+matrix.values[i][j])
-		print(newValues)
-		return Matrix(*newValues)
+	def __add__(self, matrix_or_value):
+		if isinstance(matrix_or_value, Matrix):
+			newValues = []
+			Matrix.check_dimension(self, matrix_or_value)
+			for i in range(self.dimension):
+				for j in range(self.dimension):
+					newValues.append(self.values[i][j] + matrix_or_value.values[i][j])
+			return Matrix(*newValues)
+		elif isinstance(matrix_or_value, int) or isinstance(matrix_or_value, float):
+			added_matrix = Matrix(*[matrix_or_value for i in range(self.dimension**2)])
+			return self + added_matrix
+		else:
+			raise ValueError("Can't add this to matrix :(")
+
+	def add(self, matrix_or_value):
+		return self + matrix_or_value
+
+	def  __radd__(self, matrix_or_value):
+		return self + matrix_or_value
+
+	def __iadd__(self, matrix_or_value):
+		if isinstance(matrix_or_value, Matrix):
+			newValues = []
+			Matrix.check_dimension(self, matrix_or_value)
+			for i in range(self.dimension):
+				for j in range(self.dimension):
+					self.values[i][j] += matrix_or_value.values[i][j]
+			return self
+		elif isinstance(matrix_or_value, int) or isinstance(matrix_or_value, float):
+			for i in range(self.dimension):
+				for j in range(self.dimension):
+					self.values[i][j] += matrix_or_value
+			return self
+		else:
+			raise ValueError("Can't add this to matrix :(")
+	
+	def __sub__(self, matrix_or_value):
+		if isinstance(matrix_or_value, Matrix):
+			newValues = []
+			Matrix.check_dimension(self, matrix_or_value)
+			for i in range(self.dimension):
+				for j in range(self.dimension):
+					newValues.append(self.values[i][j] - matrix_or_value.values[i][j])
+			return Matrix(*newValues)
+		elif isinstance(matrix_or_value, int) or isinstance(matrix_or_value, float):
+			subed_matrix=Matrix(*[matrix_or_value for i in range(self.dimension**2)])
+			return self - subed_matrix
+		else:
+			raise ValueError("Can't sub this to matrix :(")
+
+	def __rsub__(self, matrix_or_value):
+		if isinstance(matrix_or_value, Matrix):
+			newValues = []
+			Matrix.check_dimension(self, matrix_or_value)
+			for i in range(self.dimension):
+				for j in range(self.dimension):
+					newValues.append(matrix_or_value.values[i][j] - self.values[i][j])
+			return Matrix(*newValues)
+		elif isinstance(matrix_or_value, int) or isinstance(matrix_or_value, float):
+			subed_matrix=Matrix(*[matrix_or_value for i in range(self.dimension**2)])
+			return subed_matrix - self
+		else:
+			raise ValueError("Can't sub this to matrix :(")
+
+	def sub(self, matrix_or_value):
+		return self - matrix_or_value
+
+	def __matmul__(self, matrix_or_value):
+		if isinstance(matrix_or_value, Matrix):
+			newValues = []
+			Matrix.check_dimension(self, matrix_or_value)
+			for i in range(self.dimension):
+				for j in range(self.dimension):
+					help_value = 0.0
+					for k in range(self.dimension):
+						help_value += self.values[i][k] * matrix_or_value.values[k][j] 
+					newValues.append(help_value)
+			return Matrix(*newValues)
+		else:
+			raise ValueError("Can't mul this to matrix :(")
 
 	def __str__(self):
 		return str(self.values)
--- task.py
+++ task.py
@@ -3,9 +3,44 @@ import Matrix as matrix
 
 
 if __name__ == "__main__":
-	matrix_1 = matrix.Matrix(4,5,6,7)
-	print("matrix1 =",matrix_1)
-	matrix_2 = matrix.Matrix(2,2,2,1)
-	print("matrix2 =",matrix_2)
+	matrix_1 = matrix.Matrix(1, 1, 1, 1)
+	print("matrix1 =", matrix_1)
+
+	matrix_2 = matrix.Matrix(2, 2, 2, 2)
+	print("matrix2 =", matrix_2)
+
 	matrix_3 = matrix_2.add(matrix_1)
-	print("sum of matrix1 and matrix2 = ",matrix_3)
+	print("sum of matrix1 and matrix2 = ", matrix_3)
+	
+	matrix_3 = matrix_2 + matrix_1
+	print("sum of matrix1 and matrix2 = ", matrix_3)
+
+	matrix_3 =  2 + matrix_1
+	print("sum of matrix1 and 2 = ", matrix_3)
+
+	matrix_3 =  matrix_1 + 2
+	print("sum of 2 and matrix1 = ", matrix_3)
+
+	matrix_3 = matrix_2.sub(matrix_1)
+	print("sub matrix2 and matrix1 = ", matrix_3)
+
+	matrix_3 = matrix_1.sub(matrix_2)
+	print("sub matrix1 and matrix2 = ", matrix_3)
+
+	matrix_3 = matrix_2 - matrix_1
+	print("sub matrix2 and matrix1 = ", matrix_3)
+
+	matrix_3 = matrix_1 - matrix_2
+	print("sub matrix1 and matrix2 = ", matrix_3)
+
+	matrix_3 = matrix_1 - 1
+	print("sub matrix1 and 1 = ", matrix_3)
+
+	matrix_1 += matrix_2
+	print("matrix1+=matrix2 =", matrix_1)
+
+	matrix_1 += -2
+	print("matrix1+ = -2 =", matrix_1)
+
+	matrix_3 = matrix_1 @ matrix_2
+	print("matrix1 multiplid by matrix2 =", matrix_3)
