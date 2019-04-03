b691180e3706f69525a53f189200be128f0096ad
Michał Piwowarczyk <micpiw16@gmail.com>
2019-04-03 20:07:26 +0200

    Keep going

--- Matrix.py
+++ Matrix.py
@@ -1,10 +1,29 @@
+from math import sqrt
+
 class Matrix(object):
-	def __init__(self, *valuesInput):
-		self.values=valuesInput
+	@staticmethod 
+	def check_dimension(num_of_arguments):
+		dimension = sqrt(num_of_arguments)
+		print(dimension)
+		print("dim")
+		if dimension.is_integer():
+			return int(dimension)
+		else:
+			raise ValueError ("Wrong number of arguments")
+
+	def __init__(self, *values_input):
+		self.dimension = Matrix.check_dimension(len(values_input))
+		self.values = []
+		for i in range(self.dimension):
+			self.values.append([j for j in values_input[i*self.dimension:(i+1)*self.dimension]])
+
 	def add(self, matrix):
 		newValues = []
-		for i in range(len(self.values)):
-			newValues.append(self.values[i]+matrix.values[i])
-		return Matrix(newValues)
+		for i in range(self.dimension):
+			for j in range(self.dimension):
+				newValues.append(self.values[i][j]+matrix.values[i][j])
+		print(newValues)
+		return Matrix(*newValues)
+
 	def __str__(self):
 		return str(self.values)
